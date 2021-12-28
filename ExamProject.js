53d6967a168374e1e3964d9b73fba92969d6322d
shibuxing <shibuxing@zufe.edu.cn>
2021-12-28 20:10:38 +0800

    using express test

--- ExamProject.js
+++ ExamProject.js
@@ -1,1 +1,12 @@
-console.log("666");
+const express = require('express');
+const ejs = require('ejs');
+const app = express();
+app.use('/',express.static('html'));
+
+app.use('/', (req, res) => {
+  res.sendFile('login.html', (err)=>{
+    if(err)console.log(err);
+  })
+});
+
+app.listen(60123);
